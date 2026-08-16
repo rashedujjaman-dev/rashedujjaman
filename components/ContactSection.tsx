81075a3000d85f5844ac4b2b721d5f3333d92c81
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-08-16 23:10:09 +0600

    contact mail hide

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -9,13 +9,13 @@ const ContactSection = () => {
   return (
     <section id="contact" className='max-w-7xl mx-auto px-5 md:px-15 py-24 relative'>
         <ScrollReveal stagger={0.08}>
-            <h3 className='text-2xl md:text-3xl font-semibold text-slate-400  not-dark:text-slate-600 mb-7'>Contact</h3>
+            <h3 className='text-2xl md:text-3xl font-semibold text-slate-400  not-dark:text-slate-600 mb-5'>Contact</h3>
             <p className='text-sm text-slate-400  not-dark:text-slate-600 max-w-xl'>Have a project in mind or need a modern, high-performing website? Let’s connect and turn your ideas into a powerful digital experience.</p>
-            <div className='mt-6 text-slate-400  not-dark:text-slate-600'>
+            {/* <div className='mt-6 text-slate-400  not-dark:text-slate-600'>
                 <Link href="sincere.team@yahoo.com" className='hover:text-[#a855f7]'>sincere.team@yahoo.com</Link>
-            </div>
+            </div> */}
 
-            <div className='mt-6 flex items-center gap-4 text-white/80'>
+            <div className='mt-5 flex items-center gap-4 text-white/80'>
                <SocialLinks />
             </div>
         </ScrollReveal>
