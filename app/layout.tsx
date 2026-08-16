4b549d5280ee2b9b8890492b4459a582a1a6454f
Rashedujjaman Md <rashed.jaman.raj@gmail.com>
2026-08-16 11:43:57 +0600

    Update title in metadata for layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Rashedujjaman | Software Developer ",
+  title: "Rashedujjaman | Full-stack JavaScript developer ",
   description: "Full-Stack JavaScript Developer specializing in building modern, responsive, and scalable web applications using Next.js, React, TypeScript, Node.js,Tailwind CSS, MongoDB, and Mongoose.",
 };
 
